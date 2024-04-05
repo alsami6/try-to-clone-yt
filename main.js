2a35fa86b71e48327aecbbdb879cd73ccd2635ed
Al <alsami1209@gmail.com>
2024-04-05 22:08:30 +0600

    k

--- main.js
+++ main.js
@@ -6,3 +6,8 @@
     element.classList.remove('hidden');
 
     // I am confused I don't no whot to do next
+    // still I can not find what to do 
+    for (let index = 0; index < 100; index++) {
+        console.log(index);
+        
+    }
